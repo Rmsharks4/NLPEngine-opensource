12739e1c15d82b87c2929a90eab70981f9b4c516
Rmsharks4 <rmsharks4@gmail.com>
2019-10-22 09:18:33 +0500

    Add files via upload

--- feature_engineering/main3.py
+++ feature_engineering/main3.py
@@ -1,25 +1,66 @@
-
+import csv
 import pandas as pd
-import re
-from lupyne import engine
-from lucene import SimpleAnalyzer, Document, TermQuery, QueryParser, IndexSearcher, Term, Version
 
-df = pd.read_csv('train-data.csv')
+# APPROPRIATE OPENING DATASET
+# CONVERSATION[0:4], GREET{2], INTRODUCE_SELF[2], INTRODUCE_COMPANY[2], OFFER[2]
 
-def testKeyword(self):
-    searcher = IndexSearcher(self.directory, True)
-    t = Term("isbn", "1930110995")
-    query = TermQuery(t)
-    scoreDocs = searcher.search(query, 50).scoreDocs
-    self.assertEqual(1, len(scoreDocs), "JUnit in Action")
+from flashtext.keyword import KeywordProcessor
+import csv
 
-for text in df['Text'].unique():
-    for word in re.split('[\s,]+', text):
-        hit = Hit.cast_(word)
-        print(hit.getScore(), ':', hit.getDocument['title'])
+gr = KeywordProcessor()
 
-# for col in ['Talk', 'Polarity', 'Mode', 'Topic', 'Sp-Act']:
-#     for key in df[col].unique():
-#         if key is not None and type(key) == str:
-#             rows = df.loc[df[col] == key]
-#             rows.to_csv('data/'+col+'/'+key+'.csv', index=None)
+with open('../data/intents/GreetDialogueIntentImpl.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    for row in reader:
+        gr.add_keyword(row[0])
+
+se = KeywordProcessor()
+
+with open('../data/intents/IntroduceSelfDialogueIntentImpl.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    for row in reader:
+        se.add_keyword(row[0])
+
+co = KeywordProcessor()
+
+with open('../data/intents/IntroduceCompanyDialogueIntentImpl.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    for row in reader:
+        co.add_keyword(row[0])
+
+of = KeywordProcessor()
+
+with open('../data/intents/OfferDialogueIntentImpl.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    for row in reader:
+        of.add_keyword(row[0])
+
+df = pd.read_csv('../data/AbstractDialoguePreProcessor.csv', sep=',', encoding='utf-8')
+
+output = None
+
+for cid in df['Call_ID'].unique():
+    if cid == 1:
+        output = pd.DataFrame({'Call_ID': df['Call_ID'],
+                               'Speaker': df['Speaker'],
+                               'Data': df['WordNetLemmatizerDialoguePreProcessorImpl.SpellCheckerDialoguePreProcessorImpl'].
+                              where(df['Call_ID'] == cid)})
+        output = output.dropna()
+        output = output[:4]
+    else:
+        temp = pd.DataFrame({'Call_ID': df['Call_ID'],
+                             'Speaker': df['Speaker'],
+                             'Data': df['WordNetLemmatizerDialoguePreProcessorImpl.SpellCheckerDialoguePreProcessorImpl'].
+                            where(df['Call_ID'] == cid)})
+        temp = temp.dropna()
+        output = output.append(temp[:4], ignore_index=True)
+
+output['Greet'] = output['Data'].apply(lambda x: gr.extract_keywords(str(x)))
+
+output['IntroduceSelf'] = output['Data'].apply(lambda x: se.extract_keywords(str(x)))
+
+output['IntroduceCompany'] = output['Data'].apply(lambda x: co.extract_keywords(str(x)))
+
+output['Offer'] = output['Data'].apply(lambda x: of.extract_keywords(str(x)))
+
+print(output)
